76d0b254de2f431a0304bebc990aa1e2f7a7b94c
Administrator <Administrator@2013-20160307YL>
2017-08-19 09:42:26 +0800

    两种方法

---
+++ src/com/zhiyou100/People.java
@@ -0,0 +1,23 @@
+package com.zhiyou100;
+
+import org.eclipse.jdt.internal.compiler.batch.Main;
+
+public class People {
+
+	int age;
+	String name;
+	double sex;
+	public People(int age, String name, double sex) {
+		super();
+		this.age = age;
+		this.name = name;
+		this.sex = sex;
+	}
+	@Override
+	public String toString() {
+		return "People [age=" + age + ", name=" + name + ", sex=" + sex + ", getClass()=" + getClass() + ", hashCode()="
+				+ hashCode() + ", toString()=" + super.toString() + "]";
+	}
+	
+
+}
